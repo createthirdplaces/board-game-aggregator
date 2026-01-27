70c170d686e157a1d7a88f11415c6e26e820a712
Gulu <bponnaluri@gmail.com>
2026-01-27 15:07:17 -0500

    Add import of additional file

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+
+
 require __DIR__ . '/vendor/autoload.php';
 
+require $_SERVER['DOCUMENT_ROOT'] . "/api/api.php";
 
 echo "<h1>Board game event aggregator is under construction</h1>";
+
+callApi();
