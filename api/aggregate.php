a1f90af2dbbb5fcc827d52fa0a87abb0bb66d740
Gulu <bponnaluri@gmail.com>
2026-01-27 15:51:21 -0500

    Add loading indicator

---
+++ api/aggregate.php
@@ -0,0 +1,11 @@
+<?php
+
+
+sleep(2);
+echo "
+
+<p> Data has loaded</p>
+
+"
+
+?>
--- index.php
+++ index.php
@@ -1,10 +1,35 @@
-<?php
+<html>
+  <head>
+	</head>
+	<body>
+  
+		<h1>Board game aggregator</h1>
+		<div id="results">
+	    <p>Loading</p>	
+		</div>
+		<?php
+			require __DIR__ . '/vendor/autoload.php';
 
 
-require __DIR__ . '/vendor/autoload.php';
+      //TODO: Check cache before calling API
+      
+			echo '
+      <script>
+		    const url = "/api/aggregate.php"	
+				const data = fetch(url,{
+           "Content-Type": "text/html; charset=UTF-8"
+					}).then((response)=>{
+						if (!response.ok){
+							throw new Error(`Response status: ${response.status}`);
+						}
+					  console.log(response);	
+						response.text().then((result)=>{
+							document.getElementById(`results`).innerHTML = result;
+						});
 
-require $_SERVER['DOCUMENT_ROOT'] . "/api/api.php";
+				});
+			</script>';
+    ?>
 
-echo "<h1>Board game event aggregator is under construction</h1>";
-
-callApi();
+  </body>
+</html>
